f579cf57b6638bde33040e275924238c422aa06a
Diego Aguiar <diegoafaguiar@gmail.com>
2024-03-21 11:31:32 -0300

    Add image domain to next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+images: {
+  domains: ['i0.wp.com'],
+},
+};
 
 export default config;
