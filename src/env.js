fbb7893a6e2bde887cc6005308432dfe0af1bc60
Diego Aguiar <diegoafaguiar@gmail.com>
2024-03-21 11:33:33 -0300

    Add WP_POSTS_URL environment variable to server and runtimeEnv

--- src/env.js
+++ src/env.js
@@ -8,6 +8,7 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    WP_POSTS_URL: z.string().min(1),
   },
 
   /**
@@ -25,6 +26,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    WP_POSTS_URL: process.env.WP_POSTS_URL,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
