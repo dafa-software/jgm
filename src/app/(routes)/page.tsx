7149e330e86e46a8cdb9215e139308bf1ab08f12
CaimTDL <caiobarber272@gmail.com>
2024-03-25 17:27:32 -0300

    feat: add components card services

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import ServicesCard from "../_components/cards/services";
+
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-blue-main text-white">
@@ -5,6 +8,16 @@ export default async function Home() {
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           JGM
         </h1>
+        {}
+        <div>
+          <ServicesCard
+            title={"JGMFLEX"}
+            description={
+              "Diversos serviços por assinatura e opções de planos para você escolher o que melhor te atende. E com os melhores profissionais!"
+            }
+            action1Text={"Share"}
+          />
+        </div>
       </div>
     </main>
   );
--- src/app/_components/cards/services.tsx
+++ src/app/_components/cards/services.tsx
@@ -1,3 +1,37 @@
-export function ServicesCard() {
-  return <div> </div>;
+import React from "react";
+
+interface ServicesCardProps {
+  title: string;
+  description: string;
+  action1Text: string;
+  onAction1Click?: () => void;
 }
+
+const ServicesCard: React.FC<ServicesCardProps> = ({
+  title,
+  description,
+  action1Text,
+
+  onAction1Click,
+}) => {
+  return (
+    <div className="h-80 max-w-md rounded-lg bg-white p-5 shadow-md">
+      <div className="text-center">
+        <h5 className="text-lg font-bold text-red-500">{title}</h5>
+        <br></br>
+        <p className="text-sm text-black">{description}</p>
+      </div>
+
+      <div className="flex justify-center">
+        <button
+          className=" mt-2  rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+          onClick={onAction1Click}
+        >
+          {action1Text}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesCard;
