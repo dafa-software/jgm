2f042c0b9f92e7d29504ea2fac3cbe044e14bae2
Ed Castro <ed.sdr@outlook.com>
2024-03-25 16:38:09 -0300

    feat: landing page structure

--- src/app/(lps)/pos-obra/page.tsx
+++ src/app/(lps)/pos-obra/page.tsx
@@ -1,3 +1,58 @@
+import { HeroCard } from "~/app/_components/cards/hero";
+import WhyCard from "~/app/_components/cards/why";
+import Container from "~/app/_components/container";
+import Form from "~/app/_components/form";
+import Section from "~/app/_components/section";
+import Title from "~/app/_components/title";
+import { LandingPageWhData } from "~/data";
+
 export default function PosObra() {
-  return <div>wip</div>;
+  return (
+    <main>
+      <HeroCard
+        title="Limpeza pós-obra"
+        text="Procurando uma empresa de limpeza pós-obra de qualidade e com profissionais especializados? Conheça a melhor empresa de limpeza pós-obra do Rio de Janeiro!"
+        contentBottom={
+          <div className="flex flex-col gap-6 md:flex-row">
+            <div>
+              <p className="font-bold">WhatsApp</p>
+              <p>(21)97737-8854</p>
+            </div>
+            <div>
+              <p className="font-bold">E-mail</p>
+              <p>comercial@jgmservicos.com.br</p>
+            </div>
+          </div>
+        }
+        contentRight={<Form variant="thin" />}
+      />
+      <div className="bg-white">
+        <Container>
+          <Section>
+            <div className={`grid grid-cols-1 gap-12 md:grid-cols-3`}>
+              {LandingPageWhData.map((props, index) => (
+                <WhyCard key={index} {...props} />
+              ))}
+            </div>
+          </Section>
+        </Container>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-2 bg-blue-main p-6 font-bold md:flex-row md:gap-6">
+        <h2 className="text-xl text-white">Pronto para começar?</h2>
+        <button className="rounded-md bg-white p-3 px-6 text-blue-main transition duration-300 hover:bg-blue-100">
+          Solicite um orçamento
+        </button>
+      </div>
+      <div className="bg-white">
+        <Container>
+          <Section>
+            <Title
+              title="Nosso time JGM"
+              text="Conheça um Pouco Mais Sobre os Profissionais que Compõem Nossa Equipe"
+            />
+          </Section>
+        </Container>
+      </div>
+    </main>
+  );
 }
--- src/data/index.tsx
+++ src/data/index.tsx
@@ -1,3 +1,11 @@
+import {
+  PeopleIcon,
+  SearchFolderIcon,
+  ShieldIcon,
+  StarsIcon,
+  ToolIcon,
+} from "~/app/_components/icons";
+
 // Header
 export const NavigationData = [
   {
@@ -100,4 +108,37 @@ export const FooterTextData = [
   { title: "Empresa", data: CompanyData },
   { title: "Jurídico", data: LegalData },
   { title: "Contato", data: ContactData },
+];
+
+export const LandingPageWhData = [
+  {
+    icon: <ToolIcon className="h-7 w-7" color="white" />,
+    color: "bg-[#FF6565]",
+    title: "Levamos todo o material e equipamentos",
+    text: "Você não precisa se preocupar com nada! Levamos exatamente tudo o que precisar pra realizar a sua limpeza pós-obra.",
+  },
+  {
+    icon: <ShieldIcon className="h-7 w-7" color="white" />,
+    color: "bg-[#9CDDFF]",
+    title: "Seguro contradanos",
+    text: "Oferecemos um seguro para proteger nossos clientes caso haja qualquer imprevisto, durante o serviço prestado.",
+  },
+  {
+    icon: <StarsIcon className="h-7 w-7" color="white" />,
+    color: "bg-[#FFBC58]",
+    title: "Excelência e Experiência",
+    text: "São mais de 17 anos atuando no mercado de limpeza com excelência e profissionalismo, que irão atender todas as suas expectativas!",
+  },
+  {
+    icon: <SearchFolderIcon className="h-7 w-7" color="white" />,
+    color: "bg-[#74E091]",
+    title: "Monitoramento durante toda a limpeza por um líder",
+    text: "Sua limpeza será monitorada por um líder que coordenará toda a equipe para que a limpeza seja no nosso padrão de excelência e termine no prazo estabelecido.",
+  },
+  {
+    icon: <PeopleIcon className="h-7 w-7" color="white" />,
+    color: "bg-[#786EFE]",
+    title: "Equipe treinada e especializada",
+    text: "Temos uma equipe especialista apenas em pós-obra. Oferecemos treinamentos mensais para aprimoramento de técnicas de limpeza e profissionalização.",
+  },
 ];
