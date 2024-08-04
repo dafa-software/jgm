a2b46b38036ca682f176c018d0360836508842f8
diegoafaguiar <diegoafaguiar@gmail.com>
2024-08-04 14:47:05 -0300

    feat: fonts, analytics and google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 import { TRPCReactProvider } from "~/trpc/react";
 import Whatsapp from "./_components/whatsapp";
+// import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+
 export const runtime = "edge";
 
 const inter = Inter({
@@ -24,10 +27,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className={inter.className}>
       <body className={`font-sans ${inter.variable}`}>
+        {/* <GoogleAnalytics gaId="G-2ZVEPQKJ990" /> */}
+        {/* <GoogleTagManager gtmId="GTM-2ZVEPQKJ990" /> */}
         <Whatsapp />
         <TRPCReactProvider>{children}</TRPCReactProvider>
+        <Analytics />
       </body>
     </html>
   );
