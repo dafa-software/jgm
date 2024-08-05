503bee59160e25c0b661a94158594ea3edd73890
diegoafaguiar <diegoafaguiar@gmail.com>
2024-08-05 11:03:31 -0300

    fix: whatsapp widget

--- src/app/_components/whatsapp.tsx
+++ src/app/_components/whatsapp.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Whatsapp = () => {
   return (
-    <Link href="https://api.whatsapp.com/send?phone=21964127226">
+    <Link href="https://api.whatsapp.com/send?phone=5521964127226">
       <div className="absolute bottom-0 right-16 z-50 md:bottom-16">
         <div className="fixed flex flex-col items-center justify-start rounded-full bg-green-500 p-3 text-white shadow-xl md:flex">
           <Image src="/logos/whatsapp.svg" alt="" width={28} height={28} />
